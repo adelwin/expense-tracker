fc325ef5b9d3e037596a5fc02404e2a79a9041e5
Adelwin Handoyo <adelwin@Pruone-1s-MacBook-Pro.local>
2015-08-29 22:23:43 +0800

    Initial Commit

---
+++ ExpenseTracker/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ExpenseTracker
+//
+//  Created by Adelwin Handoyo on 29/08/2015.
+//  Copyright (c) 2015 Adelwin Handoyo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ExpenseTracker/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ExpenseTracker
+//
+//  Created by Adelwin Handoyo on 29/08/2015.
+//  Copyright (c) 2015 Adelwin Handoyo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
